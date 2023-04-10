b23f0f719e8940323577efb943f0d8e24594d1ad
GovardFilipsovich <ricardrajonhart@gmail.com>
2023-04-10 12:47:47 +0300

    Added error output and saving user-entered data

--- Gaykin_lab2/Train/views.py
+++ Gaykin_lab2/Train/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import django.contrib.messages as messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.shortcuts import render
@@ -7,13 +8,37 @@ from django.views import View
 
 
 class MainView(View):
+
     def get(self, request, *args, **kwargs):
+        # Create context for data saving
+        context = {}
+
+        # Updates data
+        context["mail"] = request.session.get('mail')
+        context["quest"] = request.session.get('quest')
+
+        # Render index page
         return render(
             request,
-            'Train/index.html'
+            'Train/index.html',
+            context
         )
 
+
 def my_form(request):
+    mail = request.POST["ADRESS"]
+    quest = request.POST["QUEST"]
     if request.POST:
-        mail = request.POST["ADRESS"]
+        if 1:
+            # call error message
+            messages.error(request, "Error")
+
+            # send to sessiona data that user enter
+            request.session["mail"] = mail
+            request.session["quest"] = quest
+
+            # redirect to index page
+            return redirect("/")
+
+        # Message to user his message send successfully
         return HttpResponse("Thanks! The answer will be sent to the mail %s" % mail)
